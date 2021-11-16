0e24b65af6f6f75c196d7939a767a119d0374856
Francesco Bianco <info.francescobianco@gmail.com>
2021-11-16 12:06:38 +0100

    Update webrequest-button.php

--- webrequest-button.php
+++ webrequest-button.php
@@ -2,10 +2,27 @@
 
 header("Content-type: image/png");
 $string = "Visit webrequest.cc";
-$im = imagecreate(110, 20);
-$orange = imagecolorallocate($im, 220, 210, 60);
-$px     = (imagesx($im) - 7.5 * strlen($string)) / 2;
+$img = imagecreate(50, 100);
+$px     = (imagesx($img) - 7.5 * strlen($string)) / 2;
 
-imagestring($im, 3, $px, 9, $string, $orange);
-imagepng($im);
-imagedestroy($im);
+
+$x1 = 0;
+$y1 = 0;
+$x2 = 50;
+$y2 = 100;
+$r = 5
+$color  = imagecolorallocate($img, 220, 210, 60);
+$r = min($r, floor(min(($x2 - $x1) / 2, ($y2 - $y1) / 2)));
+// render corners
+imagefilledarc($img, $x1 + $r, $y1 + $r, $r * 2, $r * 2, 180, 270, $color, IMG_ARC_PIE);
+imagefilledarc($img, $x2 - $r, $y1 + $r, $r * 2, $r * 2, 270, 0, $color, IMG_ARC_PIE);
+imagefilledarc($img, $x2 - $r, $y2 - $r, $r * 2, $r * 2, 0, 90, $color, IMG_ARC_PIE);
+imagefilledarc($img, $x1 + $r, $y2 - $r, $r * 2, $r * 2, 0, 180, $color, IMG_ARC_PIE);
+// middle fill, left fill, right fill
+imagefilledrectangle($img, $x1+$r, $y1, $x2-$r, $y2, $color);
+imagefilledrectangle($img, $x1, $y1+$r, $x1+$r, $y2-$r, $color);
+imagefilledrectangle($img, $x2-$r, $y1+$r, $x2, $y2-$r, $color);
+
+//imagestring($im, 3, $px, 9, $string, $orange);
+imagepng($img);
+imagedestroy($img);
