aa55a8bc84c2e2f685b2b191a802bb760da34c9e
Francesco Bianco <info.francescobianco@gmail.com>
2021-11-16 11:35:00 +0100

    Update webrequest-button.php

--- webrequest-button.php
+++ webrequest-button.php
@@ -2,7 +2,7 @@
 
 header("Content-type: image/png");
 $string = "Visit webrequest.cc";
-$im = imagecreate(110, 20)
+$im = imagecreate(110, 20);
 $orange = imagecolorallocate($im, 220, 210, 60);
 $px     = (imagesx($im) - 7.5 * strlen($string)) / 2;
 
