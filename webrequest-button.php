ac60a3d7c08179b485fee61370526d79c598b109
Francesco Bianco <info.francescobianco@gmail.com>
2021-11-16 11:32:23 +0100

    Create webrequest-button.php

---
+++ webrequest-button.php
@@ -0,0 +1,11 @@
+<?php
+
+header("Content-type: image/png");
+$string = "Visit webrequest.cc";
+$im = imagecreate(110, 20)
+$orange = imagecolorallocate($im, 220, 210, 60);
+$px     = (imagesx($im) - 7.5 * strlen($string)) / 2;
+
+imagestring($im, 3, $px, 9, $string, $orange);
+imagepng($im);
+imagedestroy($im);
