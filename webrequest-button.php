41e8799a10343c70192e79ba73990104f50562df
Francesco Bianco <info.francescobianco@gmail.com>
2022-01-24 18:06:42 +0100

    Update webrequest-button.php

--- webrequest-button.php
+++ webrequest-button.php
@@ -2,7 +2,7 @@
 
 header("Content-type: image/png");
 $string = "Visit webrequest.cc";
-$img = imagecreate(50, 100);
+$img = imagecreate(500, 500);
 $px = (imagesx($img) - 7.5 * strlen($string)) / 2;
 
 
